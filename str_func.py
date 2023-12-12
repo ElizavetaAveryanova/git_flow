56318428fce24cbbc16678eb7503b424c16132b2
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2023-12-12 13:18:43 +0500

    Update str_func.py
    Добавлен docstring  к функции

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
 def capitalize():
+    """
+    функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами
+    """
     stroka_user = input (str())
     return stroka_user.upper()
 

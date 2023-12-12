5f4fdbae831f7a41c4539ecd73da99a5752b996f
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2023-12-12 14:51:39 +0500

    add func_title_str add func title_str

--- str_func.py
+++ str_func.py
@@ -1,5 +1,17 @@
 def capitalize():
+    """
+    функция, которая принимает на вход строку и возвращает строку со всеми заглавными буквами
+    """
     stroka_user = input (str())
     return stroka_user.upper()
 
 capitalize()
+
+def title_str():
+    """
+    функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    """
+    stroka_user = input (str())
+    return ' '.join(word.capitalize() for word in stroka_user.split())
+
+title_str()
